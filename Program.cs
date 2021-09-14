d8b18cc9ba57a44de32ee74ea8dacdf7cb1033ab
bobby3003 <roberto3003@riseup.net>
2021-09-14 16:33:37 +0700

    added code

--- Program.cs
+++ Program.cs
@@ -10,12 +10,47 @@ namespace CSCalculator
     {
         static void Main(string[] args)
         {
-            int a = 42;
-            int b = 119;
-            int c = a + b;
-            Console.WriteLine(c);
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("Type a number, and than press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number, and the press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option form the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option?");
+
+            switch (Console.ReadLine())
+            {
+
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+        
+                            case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+            break;
+                            case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            Console.Write("Press any key to close the Calculator console app...");
             Console.ReadKey();
-            Console.ReadLine();
         }
+
     }
+
+
 }
